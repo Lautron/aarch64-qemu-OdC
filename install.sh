4c65e7335f03e6b72dc3a03cbf0da43e0ded607e
Lautaro Bachmann <laurigbach@gmail.com>
2022-05-18 19:24:35 -0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "Pulling docker image..."
+sudo docker pull lautronb/aarch64-qemu:latest
+echo ""
+echo "Getting runqemu from Github..."
+curl https://raw.githubusercontent.com/Lautron/aarch64-qemu-OdC/master/runqemu -o runqemu
+chmod +x runqemu
+echo ""
+echo "Copying runqemu to ~/.local/bin"
+cp runqemu ~/.local/bin
